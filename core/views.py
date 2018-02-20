187868004261046656acd96c49785665ff4d0a52
Paulo Endoh <paulo.endoh@gmail.com>
2018-02-19 22:40:45 -0800

    Created sign up page; Started login page;

--- core/views.py
+++ core/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
 
 def index(request):
-    return render(request, 'signup.html')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+                login(request, user)
+                return redirect('index')
+        else:
+            form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
