13a86c3dd9f9c9528279ba22186533d2c211141f
Paulo Endoh <paulo.endoh@gmail.com>
2018-03-02 00:38:11 -0800

    Bootstrap on signup/login page; Updated navbar and added tabs

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -18,7 +18,7 @@ def index(request):
                 username = form.cleaned_data.get('username')
                 raw_password = form.cleaned_data.get('password1')
                 user = authenticate(username=username, password=raw_password)
-                login(request, user)
+                auth_login(request, user)
                 return redirect('index')
         else:
             form = UserCreationForm()
